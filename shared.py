362be24e38ee97ac8d46fad0b378f3f47cf15220
mreagan3200 <m.reagan3200@gmail.com>
2024-02-27 17:26:18 -0500

    changing one last thing

--- shared.py
+++ shared.py
@@ -3,7 +3,7 @@ loaded = False
 
 server_id = 1093195040320389200
 emote_server_id = 1097591742766776463
-bot_commands_id = 1094723044888547448
+bot_commands_id = 1212163580317933588
 announcements_channel_id = 1094286349550485644
 starboard_channel_id = 1102065147046015099
 reaction_roles_channel_id = 1093916970644156419
