6f0e6541bde3ef48ba35c6902eec22e4e8bc4e59
mreagan3200 <m.reagan3200@gmail.com>
2024-02-27 16:47:13 -0500

    test

--- util/RolesUtil.py
+++ util/RolesUtil.py
@@ -291,7 +291,7 @@ async def generate_leaderboard(leaderboard_list : list):
         _, _, t, b = font.getbbox(level_text)
         w = font.getlength(level_text)
         h = t-b
-        draw.text((520, (i*75) + (70-h)//2), level_text, fill='#5A6A9C', font=font)
+        draw.text((520, (i*75) + (70+h)//2), level_text, fill='#5A6A9C', font=font)
     image.save(image_path)
 
 def format_item(item_name : str):
